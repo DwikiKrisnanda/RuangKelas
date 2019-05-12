076f0c6bb7db1bff9e37f0b8d895adff0d7ee677
bilsuryananda09 <bil.suryananda@gmail.com>
2019-05-12 22:17:26 +0800

    update report, contact us, sama nama dosen

---
+++ app/src/main/java/com/example/ruangkelas/Report.java
@@ -0,0 +1,13 @@
+package com.example.ruangkelas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Report extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_layout);
+    }
+}
