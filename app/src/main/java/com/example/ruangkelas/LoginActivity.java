a46966ed93aba4ab08a1a42b33d46cf4deb22091
bilsuryananda09 <bil.suryananda@gmail.com>
2019-05-17 23:57:46 +0800

    update login

--- app/src/main/java/com/example/ruangkelas/LoginActivity.java
+++ app/src/main/java/com/example/ruangkelas/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.ruangkelas;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,17 +16,62 @@ import java.util.List;
 
 public class LoginActivity extends Activity {
 
+    /*
     List<User> listUser;
     private Button login;
     private EditText username, password;
     private int counter = 5;
     private TextView Info;
+    */
+
+    private EditText mUsername,mUserpasswd;
+    private Button mLogin;
+    private TextView mRegister;
+    private String Name,Password;
+    private SharedPreferences mSharedPreferences;
+    public static final String PREFERENCE= "preference";
+    public static final String PREF_NAME = "register_usernameText";
+    public static final String PREF_PASSWD = "register_passwordText";
     private long backPressedTime;
     private Toast backToast;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        mSharedPreferences = getSharedPreferences(PREFERENCE, Context.MODE_PRIVATE);
+        mUsername = (EditText)findViewById(R.id.loginusername);
+        mUserpasswd = (EditText)findViewById(R.id.loginpass);
+        mLogin = (Button)findViewById(R.id.btnlogin);
+        mRegister = (TextView)findViewById(R.id.daftar);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validUserData()){
+                    if(mSharedPreferences.contains(PREF_NAME) && mSharedPreferences.contains(PREF_PASSWD)){
+                        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
+                        mEditor.apply();
+                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Username atau Password Salah",Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(getApplicationContext(),"Data tidak boleh kosong",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        /*
         login=(Button)findViewById(R.id.btnlogin);
         username=(EditText)findViewById(R.id.loginusername);
         password=(EditText)findViewById(R.id.loginpass);
@@ -38,8 +85,16 @@ public class LoginActivity extends Activity {
                 validate(username.getText().toString(), password.getText().toString());
             }
         });
+        */
     }
 
+    private boolean validUserData() {
+        Name = mUsername.getText().toString().trim();
+        Password = mUserpasswd.getText().toString().trim();
+        return !(Name.isEmpty() || Password.isEmpty());
+    }
+
+    /*
     public void Daftar(View view) {
         Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
         startActivity(intent);
@@ -59,6 +114,7 @@ public class LoginActivity extends Activity {
             }
         }
     }
+    */
 
     public void onBackPressed() {
         if (backPressedTime + 2000 > System.currentTimeMillis()) {
--- app/src/main/java/com/example/ruangkelas/SignUpActivity.java
+++ app/src/main/java/com/example/ruangkelas/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.example.ruangkelas;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,22 +10,55 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SignUpActivity extends Activity {
+
     List<User> listUser;
+    /*
     EditText UsernameRegis;
     EditText EmailRegis;
     EditText PassRegis;
     EditText ConfPassRegis;
     Button register;
+    */
+    private EditText mName,mEmail,mPasswd,mConfPass;
+    private Button mRegisterBtn;
+    private String Name,Password,Email;
+    public static final String PREFERENCE= "preference";
+    public static final String PREF_NAME = "register_usernameText";
+    public static final String PREF_PASSWD = "register_passwordText";
+    public static final String PREF_EMAIL = "register_emailText";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_layout);
+        mName = (EditText)findViewById(R.id.RegistUser);
+        mEmail = (EditText)findViewById(R.id.RegistEmail);
+        mPasswd = (EditText)findViewById(R.id.RegistPass);
+        mConfPass = (EditText)findViewById(R.id.RegistConfPass);
+        mRegisterBtn = (Button)findViewById(R.id.RegistButton);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validUserData()){
+                    SharedPreferences mSharedPreference = getSharedPreferences(PREFERENCE, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor mEditor = mSharedPreference.edit();
+                    mEditor.putString(PREF_NAME,Name);
+                    mEditor.putString(PREF_PASSWD,Password);
+                    mEditor.putString(PREF_EMAIL,Email);
+                    mEditor.apply();
+                    finish();
+                }
+            }
+        });
+
+        /*
         UsernameRegis = findViewById(R.id.RegistUser);
         EmailRegis = findViewById(R.id.RegistEmail);
         PassRegis = findViewById(R.id.RegistPass);
@@ -38,8 +72,16 @@ public class SignUpActivity extends Activity {
                 checkDataEntered();
             }
         });
+        */
     }
 
+    private boolean validUserData() {
+        Name = mName.getText().toString().trim();
+        Password = mPasswd.getText().toString().trim();
+        return !(Name.isEmpty() || Password.isEmpty());
+    }
+
+    /*
     public void addUserToList (User user){
         listUser.add(user);
 
@@ -78,4 +120,5 @@ public class SignUpActivity extends Activity {
             startActivity(intent);
         }
     }
+    */
 }
